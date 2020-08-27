78f54f47ab8b729153e3474a6b45857b7ca54ffc
João Martins <joao.figueiredo.martins@canonical.com>
2020-08-27 15:54:05 -0400

    Add new purchase preview endpoint for shop

--- webapp/views.py
+++ webapp/views.py
@@ -547,7 +547,7 @@ class MachineUsage(namedtuple("MachineUsage", ["attached", "allowed"])):
         return str(self.attached)
 
 
-def post_advantage_subscriptions():
+def post_advantage_subscriptions(preview):
     if user_info(flask.session):
         advantage = AdvantageContracts(
             session,
@@ -615,9 +615,14 @@ def post_advantage_subscriptions():
     }
 
     try:
-        purchase = advantage.purchase_from_marketplace(
-            marketplace="canonical-ua", purchase_request=purchase_request
-        )
+        if not preview:
+            purchase = advantage.purchase_from_marketplace(
+                marketplace="canonical-ua", purchase_request=purchase_request
+            )
+        else:
+            purchase = advantage.preview_purchase_from_marketplace(
+                marketplace="canonical-ua", purchase_request=purchase_request
+            )
     except HTTPError:
         flask.current_app.extensions["sentry"].captureException(
             extra={"purchase_request": purchase_request}
